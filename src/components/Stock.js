c5660910e92c466ce64380a94f43f0a0a88e09f4
ashill99 <admshng@gmail.com>
2021-01-19 11:38:08 -0500

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function Stock() {
+function Stock({stock, onStockClick}) {
+
+  function handleStockClick() {
+    let id = stock.id
+    console.log('in stock')
+    onStockClick(stock)
+  }
   return (
     <div>
-      <div className="card">
+      <div className="card" key={stock.name} onClick={handleStockClick}>
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h3 className="card-title">{stock.name}</h3>
+          <p className="card-text">{stock.ticker}: {stock.price}</p>
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({stocks, onStockClick}) {
+
+  console.log(stocks)
+
+  const theStocks = stocks.map((stock) => {
+   return <div key={stock.name}>
+            <Stock 
+            stock={stock} 
+            onStockClick={onStockClick}
+            />
+          </div>
+  })
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {theStocks}
     </div>
   );
 }
